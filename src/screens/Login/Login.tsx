dea42972816ace9cba88f3cce590bc27e7efd7e6
heli b <heli.b@indianic.com>
2024-05-17 13:52:51 +0530

    feat: multi language setup.

--- src/screens/Login/Login.tsx
+++ src/screens/Login/Login.tsx
@@ -1,12 +1,14 @@
 import {View, Text} from 'react-native';
 import React from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import {useTranslation} from 'react-i18next';
 
 const Login = () => {
+  const {t} = useTranslation();
   return (
     <SafeAreaView>
       <View>
-        <Text>Login</Text>
+        <Text>{t('date')}</Text>
       </View>
     </SafeAreaView>
   );
